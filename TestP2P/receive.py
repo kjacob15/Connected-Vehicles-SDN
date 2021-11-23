951f08b68ca5c1fcc520aa8e0212b8ed44885fd0
kjacob15 <91503607+kjacob15@users.noreply.github.com>
2021-11-23 17:40:44 +0000

    modified receive code

--- TestP2P/receive.py
+++ TestP2P/receive.py
@@ -1,10 +1,18 @@
 import socket
 
-UDP_IP = "10.35.70.2"
+UDP_IP = "10.35.70.1"
 UDP_PORT = 5005
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-sock.bind(MESSAGE, (UDP_IP, UDP_PORT))
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+print("Socket created.")
+try:
+    sock.bind((UDP_IP, UDP_PORT))
+    print("Socket bind comlete.")
+except socket.error as msg:
+    print(msg)
+    print("Socket bind failed.")
+
+print("Debugging point 1")
 
 while True:
     data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
