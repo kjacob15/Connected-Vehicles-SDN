e17481e299a96ccad183e4bbb891ab09b791ad64
jackgilbride999 <jackgilbride999@gmail.com>
2021-11-29 14:19:43 +0000

    Fix port error

--- Sensors/vehicle.py
+++ Sensors/vehicle.py
@@ -149,10 +149,10 @@ def Main():
         network_number = int(sys.argv[1])
         vehicle_number = int(sys.argv[2])
         signal_host = "10.35.70." + str(network_number)
-        front_proximity_sensor_port = 33000 + (10)*vehicle_number + 1+100
-        right_proximity_sensor_port = 33000 + (10)*vehicle_number + 2+100
-        back_proximity_sensor_port = 33000 + (10)*vehicle_number + 3+100
-        left_proximity_sensor_port = 33000 + (10)*vehicle_number + 4+100
+        front_proximity_sensor_port = 33000 + (10)*vehicle_number + 1
+        right_proximity_sensor_port = 33000 + (10)*vehicle_number + 2
+        back_proximity_sensor_port = 33000 + (10)*vehicle_number + 3
+        left_proximity_sensor_port = 33000 + (10)*vehicle_number + 4
         location_sensor_port = 33000 + (10)*vehicle_number + 5
         speed_sensor_port = 33000 + (10)*vehicle_number + 6
         fuel_sensor_port = 33000 + (10)*vehicle_number + 7
