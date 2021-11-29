6205b0db9782fa78d702ac8814fbf465bc4e27ba
jackgilbride999 <jackgilbride999@gmail.com>
2021-11-29 13:09:08 +0000

    vehicle_controller -> vehicle

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
     # Get script directory
     scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
-    python $scriptDir/Sensors/vehicle_controller.py 1 &     # Vehicle controller
+    python $scriptDir/Sensors/vehicle.py 1 &     # Vehicle controller
 
     python $scriptDir/Sensors/proximity_sensor.py 1 1 &     # Front proximity sensor
     python $scriptDir/Sensors/proximity_sensor.py 1 2 &     # Right proximity sensor
