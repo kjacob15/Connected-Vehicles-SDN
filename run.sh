a33b752f84dcf79f25602955e4b075b0d25eb065
jackgilbride999 <jackgilbride999@gmail.com>
2021-11-29 14:01:18 +0000

    Fix script error

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 network_pattern='[12]'
-
 if ! [[ $1 =~ $network_pattern ]]; then
-    echo "error: first argument to script must be pi number" ?&2; exit 1
+    echo "error: first argument to script must be pi number" &2; exit 1
 fi
-
-# Get network
 network = $1
 
 (
@@ -13,13 +10,13 @@ network = $1
     # Get script directory
     scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
-    python $scriptDir/Sensors/vehicle.py $network 1 &     # Vehicle controller
+    python $scriptDir/Sensors/vehicle.py "$network" 1 &     # Vehicle controller
 
-    python $scriptDir/Sensors/proximity_sensor.py $network 1 1 &     # Front proximity sensor
-    python $scriptDir/Sensors/proximity_sensor.py $network 1 2 &     # Right proximity sensor
-    python $scriptDir/Sensors/proximity_sensor.py $network 1 3 &     # Back proximity sensor
-    python $scriptDir/Sensors/proximity_sensor.py $network 1 4 &     # Left proximity sensor
-    python $scriptDir/Sensors/location_sensor.py $network 1 5 &      # Location sensor
-    python $scriptDir/Sensors/speed_sensor.py $network 1 6 &         # Speed sensor
-    python $scriptDir/Sensors/fuel_sensor.py $network 1 7            # Fuel sensor
+    python $scriptDir/Sensors/proximity_sensor.py "$network" 1 1 &     # Front proximity sensor
+    python $scriptDir/Sensors/proximity_sensor.py "$network" 1 2 &     # Right proximity sensor
+    python $scriptDir/Sensors/proximity_sensor.py "$network" 1 3 &     # Back proximity sensor
+    python $scriptDir/Sensors/proximity_sensor.py "$network" 1 4 &     # Left proximity sensor
+    python $scriptDir/Sensors/location_sensor.py "$network" 1 5 &      # Location sensor
+    python $scriptDir/Sensors/speed_sensor.py "$network" 1 6 &         # Speed sensor
+    python $scriptDir/Sensors/fuel_sensor.py "$network" 1 7            # Fuel sensor
 )
