a33ccc0b2707612de328deab59e7a1cfe3fb3fc0
Udisha <udisha@pc-140-156.customer.ask4.lan>
2021-11-29 00:49:51 +0000

    Updated sensors and fuel codes

---
+++ Sensors/fuel_sensor.py
@@ -0,0 +1,49 @@
+import socket
+import time
+import sys 
+#import random
+
+
+try:
+    arg1 = int(sys.argv[1])
+    arg2 = int(sys.argv[2])
+    signal_port = 33000 + 10*arg1 + arg2
+
+except IndexError:
+    print("Must provide two arguments: the vehicle number and the sensor number")
+    exit()
+except ValueError:
+    print("The vehicle number and the sensor number must be valid integers.")
+    exit()
+
+signal_host = "10.35.70.2"
+#signal_port = 33090
+print("UDP target IP:", signal_host)
+print("UDP target port:", signal_port)
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) #UDP
+
+f = 0.0 
+
+try :
+    while True:
+        f = f + 0.5
+        print("Vehicle " + str(arg1) + ", Sensor " + str(arg2) + " FUEL CONSUMED = " + str(f) + "%.")
+        #x = str(f)
+        #sock.sendto(x.encode('utf-8'), (signal_host, signal_port))
+        time.sleep(0.1)
+        if(f == 0 or f < 0):
+            f = 0.0 
+            print("Vehicle " + str(arg1) + ", Sensor " + str(arg2) + " FUEL CONSUMED = " + str(f) + "%.")
+            #x = str(f)
+            #sock.sendto(x.encode('utf-8'), (signal_host, signal_port))
+        
+        if(f == 100):
+            x = "FUEL"
+            sock.sendto(x.encode('utf-8'), (signal_host, signal_port))
+            f = 0.0
+            time.sleep(5)
+except :
+    sock.close()
+
+    
